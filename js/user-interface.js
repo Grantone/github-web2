7ec755b7f028bc3ddeddc3303006bd3648ff015b
Granton Mchana <gmchana254@gmail.com>
2017-09-21 17:14:40 +0300

    additions

---
+++ js/user-interface.js
@@ -0,0 +1,16 @@
+var User =  require('./../js/user.js').userModule;
+
+$(document).ready(function () {
+
+	var currentUserObject = new User();
+	$('#searchUser').click(function (event) {
+		event.preventDefault();
+		var githubuser = $('#github-username').val();
+		$('#github-username').val("");
+		$('#repo').empty("");
+		$('#error').empty("");
+		$('.username').empty("");
+		// $('.username').text(githubuser);
+		currentUserObject.getUser(githubuser);
+	});
+});
