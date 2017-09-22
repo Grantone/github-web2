fc78acd7b402e34fe6f1c795f47f2041ae3b2988
Granton Mchana <gmchana254@gmail.com>
2017-09-22 08:06:02 +0300

    rewritten app.js file

--- build/js/app.js
+++ build/js/app.js
@@ -2,32 +2,7 @@
 exports.apiKey = "fb75f267ae9db871cbb9035d7074b596f95f6483"
 
 },{}],2:[function(require,module,exports){
-// var apiKey = require('./../.env').apiKey;
-//
-// function User() {}
-//
-// User.prototype.getUser = function(githubuser){
-//   $.get('https://api.github.com/users/' + githubuser + '/?access_token=' + apiKey).then(function(response){
-//   	 console.log(response);
-//     $('.username').html('<h2 id="githubuser">' + response.name + '</h2><img id="user-avatarImage" src=' + response.avatar_url + '>');
-//   }).fail(function(error){
-//
-//     $('#error').text(githubuser + ' could not be found, please enter a valid username');
-//
-// });
-//   $.get('https://api.github.com/users/' + githubuser + '/repos?access_token=' + apiKey).then(function(response){
-//   	for (var n = 0; n <= response.length; n++)
-//       // $('#repo').empty();
-//       if (response[n].description === null) {
-//       $('#repo').append('<li><a target="_blank" href="' + response[n].html_url + '"><p>' + response[n].name + '</p><p>Oops no description here!!!! </p></a></li>');
-//     } else {
-//       $('#repo').append('<li><a target="_blank" href="' + response[n].html_url + '"><p>' + response[n].name +'</p><p>' + response[n].description + '</p></a></li>');
-//     }
-//     // console.log(response);
-//   }).fail(function(error){
-//
-//   });
-// };
+
 var apiKey = require("./../.env").apiKey
 
 function Repo() {
@@ -54,23 +29,10 @@ Repo.prototype.getRepos = function(repoName) {
 exports.repoModule = Repo;
 
 },{"./../.env":1}],3:[function(require,module,exports){
-// var User =  require('./../js/user.js').userModule;
+// var User =  require('./../js/user.js').repoModule;
 //
-// $(document).ready(function () {
-//
-// 	var currentUserObject = new User();
-// 	$('form').submit(function (event) {
-// 		event.preventDefault();
-// 		var githubuser = $('#github-username').val();
-// 		console.log(githubuser);
-// 		$('#github-username').val("");
-// 		$('#repo').empty("");
-// 		$('#error').empty("");
-// 		$('.username').empty("");
-// 		// $('.username').text(githubuser);
-// 		currentUserObject.getUser(githubuser);
-// 	});
-// });
+
+
 var Repo = require('./../js/user.js').repoModule
 
 
