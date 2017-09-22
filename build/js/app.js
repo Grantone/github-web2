f87636b3f7b80b048b21623c557bccea06c0da8e
Granton Mchana <gmchana254@gmail.com>
2017-09-22 07:58:04 +0300

    changed the content

--- build/js/app.js
+++ build/js/app.js
@@ -1,3 +1,7 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+exports.apiKey = "fb75f267ae9db871cbb9035d7074b596f95f6483"
+
+},{}],2:[function(require,module,exports){
 // var apiKey = require('./../.env').apiKey;
 //
 // function User() {}
@@ -48,3 +52,41 @@ Repo.prototype.getRepos = function(repoName) {
 };
 
 exports.repoModule = Repo;
+
+},{"./../.env":1}],3:[function(require,module,exports){
+// var User =  require('./../js/user.js').userModule;
+//
+// $(document).ready(function () {
+//
+// 	var currentUserObject = new User();
+// 	$('form').submit(function (event) {
+// 		event.preventDefault();
+// 		var githubuser = $('#github-username').val();
+// 		console.log(githubuser);
+// 		$('#github-username').val("");
+// 		$('#repo').empty("");
+// 		$('#error').empty("");
+// 		$('.username').empty("");
+// 		// $('.username').text(githubuser);
+// 		currentUserObject.getUser(githubuser);
+// 	});
+// });
+var Repo = require('./../js/user.js').repoModule
+
+
+
+
+$(document).ready(function() {
+  $('form').submit(function(event) {
+    event.preventDefault();
+
+    var repoName = $('#goal').val()
+    console.log(repoName);
+    var data = new Repo()
+    data.getRepos(repoName)
+  });
+  // appender()
+
+});
+
+},{"./../js/user.js":2}]},{},[3]);
--- js/user-interface.js
+++ js/user-interface.js
@@ -1,16 +1,34 @@
-var User =  require('./../js/user.js').userModule;
+// var User =  require('./../js/user.js').userModule;
+//
+// $(document).ready(function () {
+//
+// 	var currentUserObject = new User();
+// 	$('form').submit(function (event) {
+// 		event.preventDefault();
+// 		var githubuser = $('#github-username').val();
+// 		console.log(githubuser);
+// 		$('#github-username').val("");
+// 		$('#repo').empty("");
+// 		$('#error').empty("");
+// 		$('.username').empty("");
+// 		// $('.username').text(githubuser);
+// 		currentUserObject.getUser(githubuser);
+// 	});
+// });
+var Repo = require('./../js/user.js').repoModule
 
-$(document).ready(function () {
 
-	var currentUserObject = new User();
-	$('#searchUser').click(function (event) {
-		event.preventDefault();
-		var githubuser = $('#github-username').val();
-		$('#github-username').val("");
-		$('#repo').empty("");
-		$('#error').empty("");
-		$('.username').empty("");
-		// $('.username').text(githubuser);
-		currentUserObject.getUser(githubuser);
-	});
+
+
+$(document).ready(function() {
+  $('form').submit(function(event) {
+    event.preventDefault();
+
+    var repoName = $('#goal').val()
+    console.log(repoName);
+    var data = new Repo()
+    data.getRepos(repoName)
+  });
+  // appender()
+
 });
